d06630f22752498a4afa005454e7c508824bf70d
Alfredo Alberto Palacios Rodriguez <yiyoaapr@gmail.com>
2025-11-13 15:52:35 -0600

    IDE files and Graph Theory Section added

---
+++ Template.cpp
@@ -0,0 +1,44 @@
+//
+// Created by yiyoa on 13/11/2025.
+//
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back()
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+#define IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define forN(n, v) for (int i = 0; i < n; i++) cin >> v[i]
+#define eraseU(v) v.erase(unique(v.begin(), v.end()), v.end())
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18 + 10;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+const vector<pii> DIR4 = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+void solve() {
+
+}
+
+int32_t main() {
+    IO
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
